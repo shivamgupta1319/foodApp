eb1aced8725762ad2cf2ef5896805fa0418d8653
shivamgupta1319 <sg247938@gmail.com>
2022-06-30 13:02:44 +0530

    complete front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react'
+import BasicCard from './components/card';
+const req = require('axios');
 function App() {
+
+
+  const [recipe_name, setRecipeName] = useState('')
+  const [data, setData] = useState({})
+  const fun = (event) => {
+    setRecipeName(event.target.value);
+    
+  }
+
+  const handleSubmit = () => {
+    req.get('http://127.0.0.1:5000/recipe/' + recipe_name).then((response) => {
+        setData(response.data);
+        
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Food Recipe</h1>
+        <input type="text" onChange={fun} value={recipe_name} name='recipe_name' placeholder='Enter Recipe Name' /><br></br><br></br><br></br>
+        <button className="button-29" role="button" type='submit' value={"submit"} onClick={() => {handleSubmit()}} >submit</button>
       </header>
+      <br></br><br></br>
+      { Object.keys(data).length > 0 && <BasicCard key={data.totalResults} recipeDetails={data}/>}
     </div>
+
+
   );
 }
 
---
+++ client/src/components/showRecipe.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+
+import CardContent from '@mui/material/CardContent';
+
+import Typography from '@mui/material/Typography';
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
+//     •
+//   </Box>
+// );
+
+export default function RecipeCard(props) {
+
+    const {aisle,consistency,name,original} = props.recipeData;
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h5" component="div"   gutterBottom>
+          Recipe Name =   {name}
+        </Typography>
+        <Typography variant="h6" component="div"   gutterBottom>
+            Items  =   {aisle}
+        </Typography>
+        <Typography variant="h6" component="div"   gutterBottom>
+          Consistency  =   {consistency}
+        </Typography>
+        <Typography variant="h6" component="div"   gutterBottom>
+          Stating  =   {original}
+        </Typography>
+        
+        
+      </CardContent>
+      
+    </Card>
+  );
+}
