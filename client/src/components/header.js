584654324331ef3049343ee5123b63c4c800873f
shivamgupta1319 <sg247938@gmail.com>
2022-07-07 18:27:19 +0530

    update

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,30 +5,37 @@ import { Input, Button, Heading, FormControl } from '@chakra-ui/react'
 import { useState } from 'react'
 import { BUTTONCOLOR, HEADERCOLOR, MARGINBOTTOM } from '../constant/constant.header';
 import { recipeData } from '../api/api.header';
+import { useForm } from "react-hook-form";
 
-// const req = require('axios');
 
 export default function Header() {
+  const { register, handleSubmit } = useForm();
   const [recipeName, setRecipeName] = useState('')
   const [data, setData] = useState({})
+  const onSubmit = item => console.log(item);
+
+
+
   const fun = (event) => {
     console.log(event.target.value);
     setRecipeName(event.target.value);
   }
+  data = await recipeData(data.recipeName);
+  setData(data);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await recipeData(recipeName)
-    setData(data);
-  }
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const data = await recipeData(recipeName)
+  //   setData(data);
+  // }  
 
   return (
     <VStack pl={10} pr={10} pt={20}>
-      <form onSubmit={(e) => handleSubmit(e)} >
+      <form onSubmit={handleSubmit(onSubmit)} >
         <FormControl align="center" isRequired marginBlock="10%">
           <Heading marginBottom={MARGINBOTTOM} fontSize={70} fontWeight='bold' color={HEADERCOLOR} align='center'>Food Recipe</Heading>
 
-          <Input marginBottom={MARGINBOTTOM} size='lg' variant='outline' type="text" onChange={fun} value={recipeName} name='recipeName' placeholder='Enter Recipe Name' />
+          <Input {...register("recipeName",{ required: true, maxLength: 20,type: 'text'})} marginBottom={MARGINBOTTOM} size='lg' variant='outline' onChange={fun} value={recipeName} name='recipeName' placeholder='Enter Recipe Name' />
           <Button colorScheme={BUTTONCOLOR} size='lg' type='submit' value={"submit"} >SEARCH</Button>
         </FormControl>
       </form>
