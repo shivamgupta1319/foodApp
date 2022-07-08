26a4c00c88e9d526fc502d22ea9139e8cf8e44f3
shivamgupta1319 <sg247938@gmail.com>
2022-07-08 10:50:07 +0530

    scroll to top

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from './routes/route';
+import ScrollToTop from './routes/scrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,11 +11,14 @@ root.render(
   <React.StrictMode>
 
     <ChakraProvider >
-    <BrowserRouter>
-    <AppRoutes/>
-    </BrowserRouter>
+
+      <BrowserRouter>
+        <ScrollToTop>
+          <AppRoutes/>
+        </ScrollToTop>
+      </BrowserRouter>
     </ChakraProvider>
-    
+
   </React.StrictMode>
 );
 
